8419327d9a761d9f5355e9e6a38b44ff19865aef
Samuel Tian <swtian@mit.edu>
2022-10-27 21:40:30 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Preferences from "./components/Preferences/Preferences";
+import { Link } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="w-full">
+			<h1>Application</h1>
+			<nav>
+				<Link to="/dashboard">Dashboard</Link>
+				<Link to="/preferences">Preferences</Link>
+			</nav>
+		</div>
+	);
 }
 
 export default App;
